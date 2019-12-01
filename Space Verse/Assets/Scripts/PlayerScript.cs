49f5b18841612de1ad41d043c39c933c3aae9090
Pruthvi <pruthvi@users.noreply.github.com>
2019-11-30 23:57:30 -0500

    Added Player control

---
+++ Space Verse/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = this.transform.forward * speed;
+        }
+
+               if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 10, 0) * Time.deltaTime * speed, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -10, 0) * Time.deltaTime * speed, Space.World);
+        }
+    }
+}
