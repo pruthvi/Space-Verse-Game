40794b2fe657f2b794ad7bd304a968b0e3bed9f3
Pruthvi <pruthvi@users.noreply.github.com>
2020-03-31 22:50:10 -0400

    Added Script to create Planets.
    Tested script that won't create generate overlapping planets and will generate planets within fixed distance

---
+++ Space Verse/Assets/Scripts/PlanetProGen/PlanetManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        // CheckLocation();
+    }
+
+
+    void CheckLocation()
+    {
+        float radius = this.gameObject.transform.localScale.x * 10;
+        Collider[] hitColliders = Physics.OverlapSphere(this.gameObject.transform.position, radius);
+        if (hitColliders.Length > 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Planet")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
