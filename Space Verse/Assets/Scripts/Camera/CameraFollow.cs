7c1618dd4f2db280d45de3596f2afaf36180a32f
Jalpen <djalpen24@gmail.com>
2020-04-04 16:46:39 -0400

    Code Refactored for SpaceShip Controller

---
+++ Space Verse/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target = null;        //  Target to follow
+    public float distance = 10;     //  Distance to the Target
+    public float cameraHeight = 5.0f;     //  Height of Camera about the target
+    public float heightDamping = 2.0f;      //  Smooth Height Damping
+    public float rotationDamping = 3.0f;    //  Smooth Rotation damping
+    public float rotationX = 30.0f;
+
+    private void LateUpdate()
+    {
+        if (!target)
+            return;
+
+        Vector3 followPosition = new Vector3(0, cameraHeight, -distance);
+        Quaternion lookRotation = Quaternion.identity;
+
+        lookRotation.eulerAngles = new Vector3(rotationX, 0, 0);
+
+        Matrix4x4 m1 = Matrix4x4.TRS(target.position, target.rotation, Vector3.one);
+        Matrix4x4 m2 = Matrix4x4.TRS(followPosition, lookRotation, Vector3.one);
+        Matrix4x4 combinedMatrix = m1 * m2;
+
+        //  Get Position from Matrix4x4
+        Vector3 position = combinedMatrix.GetColumn(3);
+
+        //  Get Rotation from Matrix4x4
+        Quaternion rotation = Quaternion.LookRotation(combinedMatrix.GetColumn(2), combinedMatrix.GetColumn(1));
+
+        Quaternion wantedRotation = rotation;
+        Quaternion currentRotation = transform.rotation;
+
+        Vector3 wantedPosition = position;
+        Vector3 currentPosition = transform.position;
+
+        currentRotation = Quaternion.Lerp(currentRotation, wantedRotation, rotationDamping * Time.deltaTime);
+        currentPosition = Vector3.Lerp(currentPosition, wantedPosition, heightDamping * Time.deltaTime);
+
+        transform.localPosition = currentPosition;
+        transform.localRotation = currentRotation;
+    }
+}
---
+++ Space Verse/Assets/Scripts/SpaceShip/SpaceshipController.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// SpaceShip Controller controls the Flying movement
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class SpaceshipController : MonoBehaviour
+{
+    #region Public Variables
+    /// <summary>
+    /// Reference to Joystick Controller
+    /// </summary>
+    [Tooltip("Reference to Joystick Controller")]
+    public Joystick joystickController = null;        
+
+    /// <summary>
+    /// Forward Speed
+    /// </summary>
+    [Header("Physics")]
+    [Tooltip("Force to push plane forwards with")] public float thrust = 100f;
+
+    /// <summary>
+    /// Turning Angle
+    /// </summary>
+    [Tooltip("Pitch, Yaw, Roll")] public Vector3 turnTorque = new Vector3(90f, 25f, 45f);
+
+    /// <summary>
+    /// Force Multiplier
+    /// </summary>
+    [Tooltip("Multiplier for all forces")] public float forceMult = 1000f;
+
+    #endregion
+
+    #region Private Variables
+    /// <summary>
+    /// Reference to Rigidbody for Physics
+    /// </summary>
+    private Rigidbody rigid;
+
+    /// <summary>
+    /// Flag to check if Spaceship can roll Override
+    /// </summary>
+    private bool rollOverride = false;
+
+    /// <summary>
+    /// Flag to check if Spaceship can pitch Override
+    /// </summary>
+    private bool pitchOverride = false;
+
+    /// <summary>
+    /// X axis rotation
+    /// </summary>
+    private float pitch = 0f;
+
+    /// <summary>
+    /// Y axis rotation
+    /// </summary>
+    private float yaw = 0f;
+
+    /// <summary>
+    /// Z axis rotation
+    /// </summary>
+    private float roll = 0f;
+
+    #endregion
+
+    /// <summary>
+    /// Caching the script reference
+    /// </summary>
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Checks for the Joystick Input Controller
+    /// </summary>
+    private void Update()
+    {
+        rollOverride = false;
+        pitchOverride = false;
+
+        float keyboardRoll = joystickController.Horizontal;
+        if (Mathf.Abs(keyboardRoll) > .25f)
+        {
+            rollOverride = true;
+        }
+
+        float keyboardPitch = joystickController.Vertical;
+        if (Mathf.Abs(keyboardPitch) > .25f)
+        {
+            pitchOverride = true;
+            rollOverride = true;
+        }
+
+        // Calculate the smooth stick inputs.
+        float autoYaw = 0f;
+        float autoPitch = 0f;
+        float autoRoll = 0f;
+
+        // Use either keyboard or smooth autopilot input.
+        yaw = autoYaw;
+        pitch = (pitchOverride) ? keyboardPitch : autoPitch;
+        roll = (rollOverride) ? keyboardRoll : autoRoll;
+    }
+
+    /// <summary>
+    /// Appling the Input Controller to SpaceShip using Rigidbody physics
+    /// </summary>
+    private void FixedUpdate()
+    {
+        // Ultra simple flight where the plane just gets pushed forward and manipulated
+        // with torques to turn.
+        rigid.AddRelativeForce(Vector3.forward * thrust * forceMult, ForceMode.Force);
+        rigid.AddRelativeTorque(new Vector3(turnTorque.x * pitch,
+                                            turnTorque.y * yaw,
+                                            -turnTorque.z * roll) * forceMult,
+                                ForceMode.Force);
+    }
+
+    /// <summary>
+    /// If SpaceShip Collides with Planet then destroy the collided Planet
+    /// </summary>
+    /// <param name="collider">
+    /// Planets
+    /// </param>
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Planet"))
+        {
+            Destroy(collider.gameObject);
+        }
+    }
+}
