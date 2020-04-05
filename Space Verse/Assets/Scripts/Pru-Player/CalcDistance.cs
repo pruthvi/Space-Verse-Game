0836f40112a6698cf4b7f5687f4065b59cff423c
Pruthvi <pruthvi@users.noreply.github.com>
2020-04-05 14:53:09 -0400

    Testing SpaceShip with new CineMachine Camera

---
+++ Space Verse/Assets/Scripts/Pru-Player/CalcDistance.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalcDistance : MonoBehaviour
+{
+  public GameObject player;
+    public float distance;
+    public int score;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = 0;
+        scoreText.text = "Score:" + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, this.transform.position);
+
+        if(distance<1)
+        {
+            float newScore = 1 / distance; 
+            score += (int)newScore;
+            scoreText.text = "Score : " + score;
+        }
+
+    }
+}
